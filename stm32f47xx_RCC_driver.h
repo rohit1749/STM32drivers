6ee6f731fb9fa275ac55cf4551016d68faa4c63d
Rohit Nair <159820725+rohit1749@users.noreply.github.com>
2024-10-30 16:10:24 +0530

    Update stm32f47xx_RCC_driver.h

--- stm32f47xx_RCC_driver.h
+++ stm32f47xx_RCC_driver.h
@@ -9,6 +9,46 @@
 #define INC_STM32F407XX_RCC_DRIVER_H_
 
 #include "stm32f407xx.h"
+/*
+ * peripheral register definition structure for RCC
+ */
+typedef struct
+{
+  __vo uint32_t CR;
+  __vo uint32_t PLLCFGR;
+  __vo uint32_t CFGR;
+  __vo uint32_t CIR;
+  __vo uint32_t AHB1RSTR;
+  __vo uint32_t AHB2RSTR;
+  __vo uint32_t AHB3RSTR;
+  uint32_t      RESERVED0;
+  __vo uint32_t APB1RSTR;
+  __vo uint32_t APB2RSTR;
+  uint32_t      RESERVED1[2];
+  __vo uint32_t AHB1ENR;
+  __vo uint32_t AHB2ENR;
+  __vo uint32_t AHB3ENR;
+  uint32_t      RESERVED2;
+  __vo uint32_t APB1ENR;
+  __vo uint32_t APB2ENR;
+  uint32_t      RESERVED3[2];
+  __vo uint32_t AHB1LPENR;
+  __vo uint32_t AHB2LPENR;
+  __vo uint32_t AHB3LPENR;
+  uint32_t      RESERVED4;
+  __vo uint32_t APB1LPENR;
+  __vo uint32_t APB2LPENR;
+  uint32_t      RESERVED5[2];
+  __vo uint32_t BDCR;
+  __vo uint32_t CSR;
+  uint32_t      RESERVED6[2];
+  __vo uint32_t SSCGR;
+  __vo uint32_t PLLI2SCFGR;
+  __vo uint32_t PLLSAICFGR;
+  __vo uint32_t DCKCFGR;
+  __vo uint32_t DCKCFGR2;
+
+} RCC_RegDef_t;
 
 //This returns the APB1 clock value
 uint32_t RCC_GetPCLK1Value(void);
