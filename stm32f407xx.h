af16dd51548603e9fe61aaa8923e37a441f3482f
Rohit Nair <159820725+rohit1749@users.noreply.github.com>
2024-10-30 16:06:32 +0530

    Update stm32f407xx.h

--- stm32f407xx.h
+++ stm32f407xx.h
@@ -197,21 +197,7 @@ typedef struct
 }EXTI_RegDef_t;
 
 
-/*
- * peripheral register definition structure for SPI
- */
-typedef struct
-{
-	__vo uint32_t CR1;
-	__vo uint32_t CR2;
-	__vo uint32_t SR;
-	__vo uint32_t DR;
-	__vo uint32_t CRCPR;
-	__vo uint32_t RXCRCR;
-	__vo uint32_t TXCRCR;
-	__vo uint32_t I2SCFGR;
-	__vo uint32_t I2SPR;
-} SPI_RegDef_t;
+
 
 
 /*
